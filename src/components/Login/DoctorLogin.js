c533c1bca1ec3d995663b893761eac96610dd33b
Ebenezhar <ebenezharselvakumar@gmail.com>
2022-09-22 11:23:05 +0530

    additonal changes

--- src/components/Login/DoctorLogin.js
+++ src/components/Login/DoctorLogin.js
@@ -11,17 +11,14 @@ function DoctorLogin() {
         }
     })
     return (
-        <div className='container'>
+        <div className='container-box'>
             <div className='title'>
-                <div className='title-box'>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-heartbeat" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <path d="M19.5 13.572l-7.5 7.428l-2.896 -2.868m-6.117 -8.104a5 5 0 0 1 9.013 -3.022a5 5 0 1 1 7.5 6.572" />
-                        <path d="M3 13h2l2 3l2 -6l1 3h3" />
-                    </svg>
-                    <h1 className="title-text">Doctor-Patient</h1>
-                </div>
-
+                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-heartbeat" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M19.5 13.572l-7.5 7.428l-2.896 -2.868m-6.117 -8.104a5 5 0 0 1 9.013 -3.022a5 5 0 1 1 7.5 6.572" />
+                    <path d="M3 13h2l2 3l2 -6l1 3h3" />
+                </svg>
+                <h1 className="title-text">Doctor-Patient</h1>
             </div>
             <h2 className='subtopic'> Login As Doctor</h2>
             <form onSubmit={formik.handleSubmit}>
@@ -45,7 +42,7 @@ function DoctorLogin() {
                         placeholder="**********"
                     />
                     {/* <button className="eye-logo"  >
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-eye" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-eye" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                             <circle cx="12" cy="12" r="2" />
                             <path d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7" />
