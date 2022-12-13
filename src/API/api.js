f97d80c9ad55c0b619bff39bc9f830e6380f3352
Ebenezhar <ebenezharselvakumar@gmail.com>
2022-12-13 17:38:25 +0530

    vercel added

--- src/API/api.js
+++ src/API/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    // baseURL: 'https://doctor-appointment-booker.herokuapp.com',
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://doctor-server-cyan.vercel.app',
+    // baseURL: 'http://localhost:5000'
 
 })
 
